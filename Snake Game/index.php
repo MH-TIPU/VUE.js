ad1da6fbf34724952572c976ef140afa58580f2c
Mosaraf Hossain Tipu <iammhtipu@gmail.com>
2019-06-09 04:12:49 +0600

    snake game

---
+++ Snake Game/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>vue.js basic</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://unpkg.com/vue"></script>
+</head>
+	<body>
+		<div id="app">
+			<snake-game></snake-game>
+		</div>
+	<script src="vue.js"></script>		
+	</body>
+</html>
+
+
+
+
+<!--------------------------------------------------->
+
+
